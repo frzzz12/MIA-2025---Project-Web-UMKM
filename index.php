bbe4a6ced2c9de3475208032e727c81df6a28fd8
kahar_crunchycloud <klencoc226@gmail.com>
2025-10-25 18:59:37 +0900

    optimisasi layout lebih fleksibel

--- index.php
+++ index.php
@@ -32,179 +32,51 @@
         </div>
      </section>
     <!-- Content 1 End -->
-
+     
     <!-- Content 2 -->
-     <section>
-        <div class=container-card>
-            <div class="primary-card">
-                <div class="sec-card">
-                        <div class="card">
-                            <div>
-                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Cozy Drink</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="cozydrink.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/bakso khas makassar daeng lalang/bakso komplit.png" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Bakso Daeng Lalang</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="baksodaenglalang.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/mocin/m1.jpg
-                                " alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Mocin</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="mocin.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/komau juice/alpukat kocok dancow.png" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Komau Juice</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="komau juice.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/nasi goreng skripsi/nasi goreng kari + katsu.jpeg" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Nasi Goreng Skripsi</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="nasigorengskripsi.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/oishii banana/o4.jpg" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Oishi Banana</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="oishi banana.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/kebab turkiyem/full sapi original.png" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Kebab Turkiyem</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="raishacraft.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/skripsi service/s2.webp" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Skripsi Service</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="skripsiservice.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/teras ryker/tr3.png" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>Teras Ryker</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="terasryker.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <div>
-                                <img src="assets/wr pangkep/wr1.png" alt="umkm1">
-                            </div>
-                            <div>
-                                <a href="">
-                                    <h3>WR Pangkep Rannu</h3>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div class="card-btn">
-                                    <a href="wr pangkep.html">
-                                        <button>Kunjungi</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                </div>
+     <section class="cards container">
+        <div class="card">
+            <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+            <div class="card-body">
+                <h3>Cozy drink</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a class="btn" href="cozydrink.html">Kunjungi</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+            <div class="card-body">
+                <h3>Cozy drink</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a class="btn" href="cozydrink.html">Kunjungi</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+            <div class="card-body">
+                <h3>Cozy drink</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a class="btn" href="cozydrink.html">Kunjungi</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+            <div class="card-body">
+                <h3>Cozy drink</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a class="btn" href="cozydrink.html">Kunjungi</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+            <div class="card-body">
+                <h3>Cozy drink</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <a class="btn" href="cozydrink.html">Kunjungi</a>
             </div>
         </div>
      </section>
     <!-- Content 2 End-->
-
 
 
 </body>
