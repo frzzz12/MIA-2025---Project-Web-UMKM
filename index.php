b8c72465c6fcb1b191d26c33080985f69c148fb1
rizkyy20 <rizkyfile12@gmail.com>
2025-10-25 15:49:23 +0800

    membuat directory masing masing umkm

--- index.php
+++ index.php
@@ -3,44 +3,206 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Dashboard</title>
 </head>
 <body>
-    <!-- Cozy Drink -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.2150676802544!2d122.51379167407802!3d-3.976100895997627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d98f379d7b9fec7%3A0xa9104bca01c90783!2sCOZY%20DRINK!5e0!3m2!1sid!2sid!4v1760857654863!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Cozy Drink -->
+    <!-- Navbar -->
+    <nav>
+        <div class="navbar">
+            <div class="navbar-h2">
+                <h2>UMKM</h2>
+            </div>
+            <div class="navbar-a">
+                <a href="">Home</a>
+                <a href="">UMKM</a>
+            </div>
+            <div class="navbar-search">
+                <input type="search">
+                <button>tombol</button>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar End -->
 
-    <!-- Makkaraja -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127366.88279972818!2d122.43396371687227!3d-3.9760967930543414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d988d483f5bc8f5%3A0xbfe87b04414728f2!2sMakkaraja%20thai%20tea!5e0!3m2!1sid!2sid!4v1760857698624!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Makkaraja -->
+    <!-- Content 1 -->
+     <section>
+        <div class="main">
+            <div class="content-1"></div>
+        </div>
+     </section>
+    <!-- Content 1 End -->
 
-    <!-- Kebab Turkiyem -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15920.091177485416!2d122.52169421699708!3d-4.015723892388991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d98930004609cad%3A0xb77f53356cd1a707!2sKebab%20turkiyem%20andonohu!5e0!3m2!1sid!2sid!4v1760857748991!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Kebab Turkiyem -->
-
-    <!-- Nasi Goreng Skripsi -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.0997650058926!2d122.52091757407815!3d-3.9999090959738246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d988dffd042935b%3A0x6b6ac8ae9bb97c72!2sNasi%20Goreng%20Skripsi!5e0!3m2!1sid!2sid!4v1760857830110!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Nasi Goreng Skripsi -->
-
-    <!-- Raisya Craft -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.6604865396926!2d104.6891606740721!3d-2.9136778970627075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e3b731569bb20b7%3A0x71f59dc81a1a6fa8!2sRaisya%20Craft%20Collection!5e0!3m2!1sid!2sid!4v1760857894391!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Raisya Craft -->
-
-    <!-- Prima -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.0612080758115!2d122.52874357407816!3d-4.007838995965904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d988d8006080cb1%3A0xa2e8d85bb9b88e5!2sSkripsi%20Service%20(SS%20GALLERY)!5e0!3m2!1sid!2sid!4v1760857944875!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Prima -->
-
-    <!-- Oishi banana -->
-     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.1599889592294!2d122.5098125740782!3d-3.987491495986236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d98f2caf97864fd%3A0xf3ceaff2b95eca3a!2sOishii%20Banana!5e0!3m2!1sid!2sid!4v1760857966959!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!--  Oishi banana-->
-
-    <!-- Creamy Corner -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.0234356148094!2d122.53959337407827!3d-4.015592395958144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d9893839e9f9fe5%3A0xfa63f8c2839a747c!2sCreamy%20Corner%20Kdi%20Anduonohu!5e0!3m2!1sid!2sid!4v1760858048717!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Creamy Corner -->
-
-    <!-- Mocin -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3980.104831600836!2d122.51548477407805!3d-3.9988658959748693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d988d84078d0c6d%3A0x5744530ede029ab1!2sMocin%20Indonesia!5e0!3m2!1sid!2sid!4v1760858075037!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-    <!-- Mocin -->
+    <!-- Content 2 -->
+     <section>
+        <div class=container-card>
+            <div class="primary-card">
+                <div class="sec-card">
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="cozydrink.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="baksodaenglalang.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="mocin.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="komau juice.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="nasigorengskripsi.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="oishi banana.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="raishacraft.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="skripsiservice.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="terasryker.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div>
+                                <img src="assets/cozy drink/alpokat kocok + ice cream.png" alt="umkm1">
+                            </div>
+                            <div>
+                                <a href="">
+                                    <h3>Cozy Drink</h3>
+                                </a>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <div class="card-btn">
+                                    <a href="wr pangkep.html">
+                                        <button>Kunjungi</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+     </section>
+    <!-- Content 2 End-->
 
 </body>
 </html>
