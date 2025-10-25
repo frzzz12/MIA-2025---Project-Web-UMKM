b281d2dc249a32a00cb77822847bd76cc5027877
Raihan <raihansaladin001@gmail.com>
2025-10-25 15:06:58 +0800

    tes lagi

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
     <!-- Mocin -->
 
      <!-- Warung Pangkep RANNU SOP SAUDARA -->
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.123456789012!2d122.58558!3d-3.999805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwNTknNTkuMyJTIDEyMsKwMzUnMDguMSJF!5e0!3m2!1sid!2sid!4v1234567890" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     <!-- Warung Pangkep RANNU SOP SAUDARA -->
 
 </body>
